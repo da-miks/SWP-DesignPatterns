350d811494ef906a218a218384bbe6608b5bd8f6
Michael <feszl.michael@gmail.com>
2022-09-30 10:29:05 +0200

    f

--- Design Patterns/Singleton.1.Lazy/Program.cs
+++ Design Patterns/Singleton.1.Lazy/Program.cs
@@ -1,23 +1,55 @@
-﻿using Singleton._1;
+﻿using System.Net.WebSockets;
+
 namespace Singleton._1.Lazy
 {
+    /// <summary>
+    /// The 'Singleton' Class
+    /// </summary>
+    public class Singleton
+    {
+        static Singleton instance = null!;
+        /*public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }*/
+
+        //Constructor is private
+        private Singleton() { }
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization
+            // Note: this is not thread safe
+            // Hint: use 'lock'
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+
     internal class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            // Constructor is private -- cannot use new
-            var s1 = Singleton.Instance();
-            var s2 = Singleton.Instance();
+            // Constructor is private --> cannot use new
+            var s1 = Singleton.Instance;
+            var s2 = Singleton.Instance;
 
-            //Test for same instance
+
+            // Test for same instance
             if (s1 == s2)
             {
                 Console.WriteLine("Objects are the same instance");
             }
         }
     }
-
 }
-
-
-
--- Design Patterns/Singleton.2.Eagerly/Program.cs
+++ Design Patterns/Singleton.2.Eagerly/Program.cs
@@ -1,11 +1,47 @@
-﻿namespace Singleton._2.Eagerly
+﻿using System.Net.WebSockets;
+
+namespace Singleton._2.Lazy
 {
+    /// <summary>
+    /// Represents a server machine
+    /// </summary>
+    /// <param name="name">The name of the server</param>
+    /// <param name="Ip">The IP Address of the server</param>
+    public record Server(string Name, string Ip);
+
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    public sealed class LoadBalancer
+    {
+        // Static members are 'eagerly initialized', that is,
+        // immediately when class is loaded for the first time.
+        // .NET guarantees thread safety for static initialization
+        private static readonly LoadBalancer instance = new();
+
+        private readonly List<Server> servers;
+
+        // Note: Constructor is private
+        private LoadBalancer()
+        {
+            // Load list of available servers
+            servers = new()
+            {
+                new(Name: "Server 1", Ip: "120.14.220.11"),
+                new(Name: "Server 2", Ip: "120.14.220.12"),
+                new(Name: "Server 3", Ip: "120.14.220.13"),
+                new(Name: "Server 4", Ip: "120.14.220.14"),
+                new(Name: "Server 5", Ip: "120.14.220.15")
+            };
+
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
-        
     }
 }
