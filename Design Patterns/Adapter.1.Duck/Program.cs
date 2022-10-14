4ed3bc88fa3545b561f14f6de4442fca37730830
da-miks <20182015@atn.ac.at>
2022-10-14 10:26:17 +0200

    14.10.2022

---
+++ Design Patterns/Adapter.1.Duck/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+
+var mallardDuck = new MallardDuck();
+var redheadDuck = new RedheadDuck();
+var duckCall = new DuckCall();
+var rubberDuck = new RubberDuck();
+var decoyDuck = new DecoyDuck();
+
+WriteLine("Duck Simulator");
+
+Simulate(mallardDuck);
+Simulate(redheadDuck);
+Simulate(duckCall);
+Simulate(rubberDuck);
+Simulate(decoyDuck);
+
+static void Simulate(IQuackable duck) => duck.Quack();
+
+#region Duck
+public interface IQuackable
+{
+    void Quack();
+}
+
+public class RubberDuck : IQuackable
+{
+    public void Quack() => WriteLine("Squeak");
+}
+public class RedheadDuck : IQuackable
+{
+    public void Quack() => WriteLine("Quack");
+}
+public class MallardDuck : IQuackable
+{
+    public void Quack() => WriteLine("Quack");
+}
+public class DuckCall : IQuackable
+{
+    public void Quack() => WriteLine("Kwak");
+}
+public class DecoyDuck : IQuackable
+{
+    public void Quack() => WriteLine("<< Silence >>");
+}
+#endregion
